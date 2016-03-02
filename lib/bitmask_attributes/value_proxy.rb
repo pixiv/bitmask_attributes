6e62a050980a7291098d5e4f478c376cae4b120a
alpaca-tc <alpaca-tc@alpaca.tc>
2016-03-02 14:25:17 +0900

    Remove space

--- lib/bitmask_attributes/value_proxy.rb
+++ lib/bitmask_attributes/value_proxy.rb
@@ -1,6 +1,6 @@
 module BitmaskAttributes
   class ValueProxy < Array
-      
+
     def initialize(record, attribute, &extension)
       @record = record
       @attribute = attribute
@@ -8,11 +8,11 @@ module BitmaskAttributes
       instance_eval(&extension) if extension
       super(extract_values)
     end
-    
+
     # =========================
     # = OVERRIDE TO SERIALIZE =
     # =========================
-    
+
     alias_method :orig_replace, :replace
     %w(push << delete replace reject! select!).each do |override|
       class_eval(<<-EOEVAL)
@@ -23,13 +23,13 @@ module BitmaskAttributes
         end
       EOEVAL
     end
-    
+
     def to_i
       inject(0) { |memo, value| memo | @mapping[value] }
     end
-  
+
     private
-    
+
       def validate!
         each do |value|
           if @mapping.key? value
@@ -39,7 +39,7 @@ module BitmaskAttributes
           end
         end
       end
-    
+
       def symbolize!
         orig_replace(map(&:to_sym))
       end
@@ -50,25 +50,25 @@ module BitmaskAttributes
         uniq!
         serialize!
       end
-    
+
       def serialize!
         @record.send(:write_attribute, @attribute, to_i)
       end
-  
+
       def extract_values
         stored = [@record.send(:read_attribute, @attribute) || 0, 0].max
         @mapping.inject([]) do |values, (value, bitmask)|
           values.tap do
             values << value.to_sym if (stored & bitmask > 0)
           end
-        end        
+        end
       end
-    
+
       def find_mapping
         unless (@mapping = @record.class.bitmasks[@attribute])
           raise ArgumentError, "Could not find mapping for bitmask attribute :#{@attribute}"
         end
       end
-      
+
   end
 end
